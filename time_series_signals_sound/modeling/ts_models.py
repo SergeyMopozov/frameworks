be198a302872bed6148faea669b1d55f0aa956bb
Sergey Morozov <mopozoffs@gmail.com>
2021-03-31 17:30:17 +0300

    Remove ipynb

---
+++ time_series_signals_sound/modeling/ts_models.py
@@ -0,0 +1,11 @@
+'''
+
+# Timeseries modeling
+Modeling is a process that contain creating and fitting model, that could be use for explain observation, forecasting future values, classification etc. Here we discuss next topics:
+
+1. Modeling aproaches for 1 step forecast
+2. Modeling aproaches for multisteps short-term forcasting
+3. Modeling aproaches for multisteps long-term forcasting
+4. External libraries Prophet, Orbit, Darts
+
+'''
