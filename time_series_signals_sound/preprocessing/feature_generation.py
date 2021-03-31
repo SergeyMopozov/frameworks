5882c61c24d56d6475520959ba589382c72dc932
Sergey Morozov <mopozoffs@gmail.com>
2021-03-31 17:24:47 +0300

    add TODO

--- time_series_signals_sound/preprocessing/feature_generation.py
+++ time_series_signals_sound/preprocessing/feature_generation.py
@@ -1,11 +1,23 @@
 '''
-This source code content function for work with time series future generation routine
+This source code content function for work with time series preprocessing and feature generation routine
+
+Multitimeseries data is an independent timeseries that represent similar processes or one process
+ from different view points (different sensors, places etc.)
+
+    1. Normalize data table structure
+    2. Time series tasks:
+        2.1 Forecasting / prediction
+        2.2 Segmentation / marking / change detection
+        2.3 Classification
+    3. Feature generation
+    4. Feature extraction
+    5. External libraries for feature generation
 '''
 
 
 def generate_lags(timeseries, window_size, terget_name=None):
     '''
-    Function get timeseries dataframe on input and return new dataframes with lagged feature
+    Function get univariate timeseries dataframe on input and return new dataframes with lagged feature
     :type window_size: int
     :type timeseries: pandas.DataFrame
     '''
@@ -18,7 +30,7 @@ def generate_lags(timeseries, window_size, terget_name=None):
 
 def generate_diffs(timeseries, window_size, terget_name=None):
     '''
-    Function get timeseries dataframe on input and return new dataframes with difference feature
+    Function get univariate timeseries dataframe on input and return new dataframes with difference feature
     :type window_size: int
     :type timeseries: pandas.DataFrame
     '''
@@ -30,7 +42,7 @@ def generate_diffs(timeseries, window_size, terget_name=None):
 
 def generate_rolling_statistics(timeseries, window_size, statistics, terget_name=None):
     '''
-    Function get timeseries dataframe on input and return new dataframes with rolling statistic feature
+    Function get univariate timeseries dataframe on input and return new dataframes with rolling statistic feature
     :type window_size: int
     :type timeseries: pandas.DataFrame
     '''
