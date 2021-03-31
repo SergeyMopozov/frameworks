89beea5c2611b7a325d00c4d6405f204fab3c1bf
Sergey Morozov <mopozoffs@gmail.com>
2021-03-31 16:34:52 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+ # Frameworks under development
+
+eda - typical steps for explore data
+
+image_video - frameforks for processing and analyse images and video streams in UAV project
+
+rainforcement_learning - frameworks for agent learning for descision making
+
+recomm_sys - different approaches for design recommendation systems, next best action etc.
+
+table_data - routine and templates for table data processing feature generetion and feature selection
+
+time_series - frameworks for processing timeseries data, modeling, forecasting, anomaly and change point detection, casual impact effect assesment
---
+++ time_series_signals_sound/preprocessing/feature_generation.py
@@ -0,0 +1,41 @@
+'''
+This source code content function for work with time series future generation routine
+'''
+
+
+def generate_lags(timeseries, window_size, terget_name=None):
+    '''
+    Function get timeseries dataframe on input and return new dataframes with lagged feature
+    :type window_size: int
+    :type timeseries: pandas.DataFrame
+    '''
+
+    result = timeseries.copy()
+    for i in range(1, window_size + 1):
+        result[f'lag_{i}'] = result.shift(i)
+    return result
+
+
+def generate_diffs(timeseries, window_size, terget_name=None):
+    '''
+    Function get timeseries dataframe on input and return new dataframes with difference feature
+    :type window_size: int
+    :type timeseries: pandas.DataFrame
+    '''
+
+    result = timeseries.copy()
+    for i in range(1, window_size + 1):
+        result[f'lag_{i}'] = result.diff(i)
+    return result
+
+def generate_rolling_statistics(timeseries, window_size, statistics, terget_name=None):
+    '''
+    Function get timeseries dataframe on input and return new dataframes with rolling statistic feature
+    :type window_size: int
+    :type timeseries: pandas.DataFrame
+    '''
+
+    result = timeseries.copy()
+    for i in range(1, window_size + 1):
+        result[f'lag_{i}'] = result.rolling(window_size).statistics
+    return result
