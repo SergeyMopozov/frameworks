0f37f21d5c3ced58cafd757365bd29ed58c1bd75
Sergey Morozov <mopozoffs@gmail.com>
2021-04-08 19:36:04 +0300

    add new functionality

---
+++ eda/eda_plots.py
@@ -0,0 +1,23 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def split_hist(data, percentile=75):
+    """
+
+    :param data:
+    :param percentile:
+    :return:
+    """
+    fst_half = data[data < np.percentile(data, percentile)]
+    snd_half = data[data >= np.percentile(data, percentile)]
+    fig, axs = plt.subplots(1, 2, figsize=(16, 6))
+    axs[0].hist(fst_half)
+    axs[0].grid()
+    axs[0].set_title(f'<{percentile} percentile')
+
+    axs[1].hist(snd_half)
+    axs[1].grid()
+    axs[1].set_title(f'>={percentile} percentile')
+    plt.show()
--- time_series_signals_sound/metrics/timeseries_metrics.py
+++ time_series_signals_sound/metrics/timeseries_metrics.py
@@ -1,63 +1,98 @@
-'''
+"""
 Timeseries  forecast model quality merics
 
 1. Simple univariate timeseries metrics (MAE, RMSE, MAPE, SMAPE)
 2. Multiple timeseries metrics
 3. Hiereachical timeseries metrics (Weighted Root Mean Squared Scaled Error (RMSSE))
 
-'''
+"""
 import numpy as np
 import pandas as pd
 
 
 def mae(y_true, y_pred):
-    '''
+    """
     Get true and predicted values and return mean absolute value
     :param y_true: array
     :param y_pred: array
     :return: int
-    '''
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
     return np.mean(np.abs(y_true - y_pred))
 
 
 def rmse(y_true, y_pred):
-    '''
+    """
     Get true and predicted values and return root mean squared error
     :param y_true: array
     :param y_pred: array
     :return:
-    '''
+    """
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
     return np.mean((y_true - y_pred) ** 2) ** 0.5
 
 
 def mape(y_true, y_pred, simple=False):
-    '''
+    """
     Get true and predicted values and return mean absolut percentage error,
     if true values contain zeroes use simple=True
     :param y_true:
     :param y_pred:
     :return:
-    '''
+    """
+    # TODO check correctness
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
     if not simple:
         return np.mean(np.abs((y_true - y_pred) / y_true))
     else:
         return np.mean(np.abs((y_true - y_pred) / np.mean(y_true)))
 
 
+def smape(y_true, y_pred, simple=False):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param simple
+    :return:
+    """
+    #TODO check correctness
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    if not simple:
+        return np.mean(np.abs((y_true - y_pred) / ((y_true + y_pred)/2)))
+    else:
+        return np.mean(np.abs((y_true - y_pred) / (np.mean(y_true + y_pred)/2)))
+
+def weighted_error(y_true, y_pred, weight, error):
+    """
+
+    :param y_true:
+    :param y_pred:
+    :param weight:
+    :param error:
+    :return:
+    """
+    #TODO write funnction that calculate wheigted error
+    return None
+
 def resampled_error(y_true, y_pred, new_freq, error):
-    '''
+    """
 
     :param y_true: pandas timeseries
     :param y_pred: array
     :param new_freq: new frequency
     :param error: error function
     :return:
-    '''
+    """
+    #TODO  add check on pandas sereis or create a series with datetime index
     y_pred = pd.Series(y_pred, index=y_true.index)
     y_pred_new = y_pred.resample(new_freq).sum()
     y_true_new = y_true.resample(new_freq).sum()
 
     return error(y_true_new, y_pred_new)
-
 
 
