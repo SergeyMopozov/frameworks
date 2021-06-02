fa2d4a6261172695fd52fdbbbe1d75ccd1bdee64
Sergey Morozov <mopozoffs@gmail.com>
2021-06-02 10:40:19 +0300

    change functions interface

--- visualization/eda_plots/eda.py
+++ visualization/eda_plots/eda.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def split_hist(data, percentile=75):
+def split_hist(data, bins=20, percentile=75):
     """
 
     :param data:
@@ -13,11 +13,11 @@ def split_hist(data, percentile=75):
     fst_half = data[data < np.percentile(data, percentile)]
     snd_half = data[data >= np.percentile(data, percentile)]
     fig, axs = plt.subplots(1, 2, figsize=(16, 6))
-    axs[0].hist(fst_half)
+    axs[0].hist(fst_half, bins=bins)
     axs[0].grid()
     axs[0].set_title(f'<{percentile} percentile')
 
-    axs[1].hist(snd_half)
+    axs[1].hist(snd_half, bins=bins)
     axs[1].grid()
     axs[1].set_title(f'>={percentile} percentile')
     plt.show()
